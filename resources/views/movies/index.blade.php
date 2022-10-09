313da88757c569d627a2f96f569f43daa9c175ff
Mirjana-V <tmirjana7@gmail.com>
2022-10-09 23:46:02 +0200

    Model Movie and MoviesController

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,9 @@
+<h3>Movies</h3>
+<ul>
+    @foreach($movies as $movie)
+        <li class="list-group-item">
+            <h2><a href="{{ route('single-movie-page',['id'=>$movie->id]) }}">{{ $movie->title }}</a></h2>
+            <h2>{{ $movie->storyline }}</h2>
+        </li>
+    @endforeach
+</ul>
